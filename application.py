108b062c39cf9577f08de2b3e2a9e21ecda0eb13
UDITH UTHAMAN <37138495+udithuthaman@users.noreply.github.com>
2021-02-11 14:40:22 +0530

    Update application.py
    another-test change

--- application.py
+++ application.py
@@ -67,7 +67,7 @@ welcome = """
 
     text-align: right;
     padding-top: 11em;
-    background-color: #1BA86D;
+    background-color: blue;
     background-image: -moz-radial-gradient(left top, circle, #6AF9BD 0%, #00B386 60%);
     background-image: -webkit-gradient(radial, 0 0, 1, 0 0, 500, from(#6AF9BD), to(#00B386));
   }
